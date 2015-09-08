69f4cdf2cab5c2e1f516f663eda6e3bd32496ec1
paulosergioduff <paulosergioduff@gmail.com>
2015-09-07 23:11:19 -0300

    Create ide.class.php

---
+++ class/ide.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class CamposIdeNFe
+
+                {
+
+public $ideinicio; //"<ide>";
+public $cUF; // ="<cUF>11</cUF>";
+public $cNF; // ="<cNF>09399701</cNF>";
+public $natOP; //"<natOp>VENDA NO ESTADO MERC.ADQ.TERCEIROS</natOp>";
+public $inPag; //"<indPag>0</indPag>";
+public $mod; //"<mod>55</mod>";
+
+public $serie; //"<serie>1</serie>";
+             public $nNF; //"<nNF>488</nNF>";
+                public $dEmi; //"<dEmi>2010-12-02</dEmi>";
+                public $dSaiEn; //"<dSaiEnt>2010-12-02</dSaiEnt>";
+                public $hSaiEnt; //"<hSaiEnt>17:04:25</hSaiEnt>";
+                public $tpNF; //"<tpNF>1</tpNF>";
+                public $cMunFG; //"<cMunFG>1100205</cMunFG>";
+                public $tpImp; //"<tpImp>1</tpImp>";
+                public $tpEmis; //"<tpEmis>1</tpEmis>";
+                public $cDV; //"<cDV>7</cDV>";
+                public $tpAmb; //"<tpAmb>2</tpAmb>";
+                public $finNFe; //"<finNFe>1</finNFe>";
+                public $procEmi; //"<procEmi>0</procEmi>";
+                public $verProc; //"<verProc>1.0.0.0</verProc>";
+            public $idefim; //"</ide>";
+
+        }
+
+            ?>
