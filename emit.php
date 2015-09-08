9d8a4adad892ca98b373adbf26ec7d60ebc6178b
paulosergioduff <paulosergioduff@gmail.com>
2015-09-07 23:23:52 -0300

    Create emit.php

---
+++ emit.php
@@ -0,0 +1,30 @@
+<?php 
+
+include "class/emit.class.php";
+
+$emit = new  CamposEmitNFe;
+
+
+ echo $emit->emitinicio = "<emit>";
+                echo $emit->CNPJ = "<CNPJ>84613439000180</CNPJ>";
+                echo $emit->xNome = "<xNome>MicroLine Software</xNome>";
+                echo $emit->xFant = "<xFant>MicroLine Software</xFant>";
+                echo $emit->enderEmitinicio = "<enderEmit>";
+                    echo $emit->xLgr = "<xLgr>Av. Rafael Vaz e Silva</xLgr>";
+                    echo $emit->nro = "<nro>003091</nro>";
+                    echo $emit->xBairro = "<xBairro>LIBERDADE</xBairro>";
+                    echo $emit->cMun = "<cMun>1100205</cMun>";
+                    echo $emit->xMun = "<xMun>PORTO VELHO</xMun>";
+                    echo $emit->UF = "<UF>RO</UF>";
+                    echo $emit->CEP = "<CEP>78904120</CEP>";
+                    echo $emit->cPais = "<cPais>1058</cPais>";
+                    echo $emit->xPais = "<xPais>BRASIL</xPais>";
+                    echo $emit->fone = "<fone>6933213610</fone>";
+                echo $emit->enderEmitfim = "</enderEmit>";
+                echo $emit->IE = "<IE>00000000415138</IE>";
+                echo $emit->IM = "<IM>999911111</IM>";
+                echo $emit->CNAE = "<CNAE>4645101</CNAE>";
+                echo $emit->CRT = "<CRT>1</CRT>";
+            echo $emit->emitfim = "</emit>";
+
+            ?>
