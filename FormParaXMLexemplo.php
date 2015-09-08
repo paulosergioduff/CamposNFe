23f1d4d6909f2851f38296b6d2a56c57c67ca396
paulosergioduff <paulosergioduff@gmail.com>
2015-09-08 11:42:50 -0300

    Update FormParaXMLexemplo.php

--- FormParaXMLexemplo.php
+++ FormParaXMLexemplo.php
@@ -1,19 +1,42 @@
 <?php
-
-include "main.php";
+include "cabecalho.php";
 
 ?>
+
+<html>
+<head>
+<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
 <hr>
-<center>
-<h1>Demonstração de obtenção de CampoNFe</h1>
+<h1>Exemplo de obtenção de CampoNFe</h1>
+<form action="gerarXML.php" method="POST">
 
-<form action="geraXML.php" method="POST">
+	<select name="pagina">
+		<option value"nfe-teste.xml">nfe-teste.xml</option>
+	</select><br>
 
-	<p><textarea rows="4" cols="50" name="conteudo"><?php 
-	
-echo $cabecalho->xmlversion;
+<select name="funcao">
 
-?></textarea>
+	<option value="Update">Atualizar</option>
+	<option value="Apagar">Apagar</option>
+	<option value="LeLinha">LeLinha</option>
+	<option value="EscreveNoFinal">EscreveNoFinal</option>
+	<option value="EscreveNoFinal">EscreNoInicio</option>
+
+</select>
+
+	<p><textarea rows="4" cols="50" name="conteudo">
+<?php 
+echo $cabecalho->xmlversion;// Exemplo de obtenção de campo!
+ ?>
+
+</textarea>
 <p><input type="submit" value="Enviar comando">
 </form>
 <p>Resultado:<br>
+<textarea>
+	
+	<?php if (file_exists('nfe-teste.xml')) { include "nfe-teste.xml"; }; ?>
+</textarea>
