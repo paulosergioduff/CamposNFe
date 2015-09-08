48b48779cf752195d9d1d8acf2850aa55d5634c8
paulosergioduff <paulosergioduff@gmail.com>
2015-09-08 11:44:03 -0300

    Update gerarXML.php

--- gerarXML.php
+++ gerarXML.php
@@ -1,26 +1,20 @@
-<?php error_reporting(0);
+<?php 
 
 include "class/woop.class.php";
-include "main.php";
+//include "FormParaXMLexemplo.php";
 
-
-echo "<hr>";
-
-$_POST['funcao'];
+$app = $_POST['funcao'];
 
 	$criarpagina = new criarpagina; /* A função de usar classes e objetos ao invés de variáveis estruturais
 	tem mais a ver com didática e organização do código*/
 
-	$criarpagina->pagina = 'nfe-teste.xml';
+	$criarpagina->pagina = $_POST['pagina'];
 	$criarpagina->conteudo = $_POST['conteudo'];
-	$criarpagina->$app();
-	echo "<p>Resultado:<br>
-	<textarea rows='4' cols='50'>";
-	include "nfe-teste.xml";
-	echo "</textarea><br>";
-	
+	//$criarpagina->arquivoalvo = ';
+	$criarpagina->$app();	
+
 ?> 
-
-
-
-
+<p>Resultado<br>
+<textarea>
+	<?php if (file_exists('nfe-teste.xml')) { include 'nfe-teste.xml';}; ?>
+</textarea>
