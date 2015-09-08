1b47d15e1f681c1bc5318a56abcbee37d4116146
paulosergioduff <paulosergioduff@gmail.com>
2015-09-07 23:22:41 -0300

    Create emit.class.php

---
+++ class/emit.class.php
@@ -0,0 +1,30 @@
+<?php 
+
+class CamposEmitNFe
+
+                {
+                    
+ public $emitinicio; // "<emit>";
+                public $CNPJ; // "<CNPJ>84613439000180</CNPJ>";
+                public $xNome; // "<xNome>MicroLine Software</xNome>";
+                public $xFant; // "<xFant>MicroLine Software</xFant>";
+                public $enderEmitinicio; // "<enderEmit>";
+                    public $xLgr; // "<xLgr>Av. Rafael Vaz e Silva</xLgr>";
+                    public $nro; // "<nro>003091</nro>";
+                    public $xBairro; // "<xBairro>LIBERDADE</xBairro>";
+                    public $cMun; // "<cMun>1100205</cMun>";
+                    public $xMun; // "<xMun>PORTO VELHO</xMun>";
+                    public $UF; // "<UF>RO</UF>";
+                    public $CEP; // "<CEP>78904120</CEP>";
+                    public $cPais; // "<cPais>1058</cPais>";
+                    public $xPais; // "<xPais>BRASIL</xPais>";
+                    public $fone; // "<fone>6933213610</fone>";
+                public $enderEmitfim; // "</enderEmit>";
+                public $IE; // "<IE>00000000415138</IE>";
+                public $IM; // "<IM>999911111</IM>";
+                public $CNAE; // "<CNAE>4645101</CNAE>";
+                public $CRT; // "<CRT>1</CRT>";
+            public $emitfim; // "</emit>";
+
+                    }
+            ?>
